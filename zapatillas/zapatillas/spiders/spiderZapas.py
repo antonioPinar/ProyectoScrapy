3a2ff6e1517fa575f3a9dddbaf0b8785eb581ac4
AntonioPinar <antoniopinar2@gmail.com>
2022-05-19 16:14:44 +0200

    añadida funcion de parsear datos

--- zapatillas/zapatillas/spiders/spiderZapas.py
+++ zapatillas/zapatillas/spiders/spiderZapas.py
@@ -1,7 +1,19 @@
 import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import Rule, CrawlSpider
+from zapatillas.items import ZapatillasItem
 
 class crawleoZapas(scrapy.Spider):
     name = "zapatillas"
-    allowed_domains = ["www.nike.com/es"]
-    start_urls = ["https://www.nike.com/es/launch?s=upcoming"]
+    allowed_domains = ['nude-project.com']
+    start_urls = ['https://nude-project.com/collections/t-shirts-nude-project/products/pretty-tee-baby-blue']
+    zapas = []
     
+
+    #funciones
+    def parse_item(self, response):
+        
+        linea = response.xpath("//section//aside/div/div/h1/text()").get()
+        yield {'dato': linea}
+
+        
