16330f121774e9ff06e4be4d1f2d406b0db76792
AntonioPinar <antoniopinar2@gmail.com>
2022-05-20 11:31:40 +0200

    modificacion funcion de parseo

--- scrapeo_ropa/scrapeo_ropa/spiders/spiderPrendas.py
+++ scrapeo_ropa/scrapeo_ropa/spiders/spiderPrendas.py
@@ -1,4 +1,3 @@
-from requests import Response
 import scrapy
 
 class spiderPrendas(scrapy.Spider):
@@ -7,5 +6,5 @@ class spiderPrendas(scrapy.Spider):
     start_urls = ['https://nude-project.com/collections/t-shirts-nude-project/products/pretty-tee-baby-blue']
 
     #funciones
-    def parseoPrendas(self, response):
+    def parse(self, response):
         nombre = response.xpath('//h1/text()').get()
