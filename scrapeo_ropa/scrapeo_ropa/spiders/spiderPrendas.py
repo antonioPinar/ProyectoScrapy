36d828a8df7cd61649d7fae72a16a67bc06b9b95
AntonioPinar <antoniopinar2@gmail.com>
2022-05-23 15:54:35 +0200

    añadimos funcionalidades al metodo parse()

--- scrapeo_ropa/scrapeo_ropa/spiders/spiderPrendas.py
+++ scrapeo_ropa/scrapeo_ropa/spiders/spiderPrendas.py
@@ -2,9 +2,13 @@ import scrapy
 
 class spiderPrendas(scrapy.Spider):
     name = 'scrapeo_ropa'
-    allowed_domains = ['nude-project.com']
-    start_urls = ['https://nude-project.com/collections/t-shirts-nude-project/products/pretty-tee-baby-blue']
+    allowed_domains = ['www.nike.com/es']
+    start_urls = ['https://www.nike.com/es/launch?s=upcoming']
 
     #funciones
     def parse(self, response):
-        nombre = response.xpath('//h1/text()').get()
+        nombre = response.xpath('//figure//div/figcaption/div/div/div/h6/text()').get()
+
+        yield{
+            'Titulos' : nombre
+        }
