bd66e7b758a4a249c3404d61f7933536ec85c4f6
AntonioPinar <antoniopinar2@gmail.com>
2022-05-23 17:22:50 +0200

    añadimos funcionalidades al metodo parse()

--- scrapeo_ropa/scrapeo_ropa/spiders/spiderPrendas.py
+++ scrapeo_ropa/scrapeo_ropa/spiders/spiderPrendas.py
@@ -1,14 +1,28 @@
 import scrapy
+from scrapy.linkextractors import LinkExtractor
 
 class spiderPrendas(scrapy.Spider):
     name = 'scrapeo_ropa'
-    allowed_domains = ['www.nike.com/es']
+    allowed_domains = ['www.nike.com']
     start_urls = ['https://www.nike.com/es/launch?s=upcoming']
+
+    productos = []
 
     #funciones
     def parse(self, response):
-        nombre = response.xpath('//figure//div/figcaption/div/div/div/h6/text()').get()
+        links = response.xpath("//figure/div/div/a/@href").getall()
 
-        yield{
-            'Titulos' : nombre
+        #recorremos todas las direcciones de los productos
+        for link in links:
+            url = "https://www.nike.com"+ link
+            yield scrapy.Request(url, callback= self.parse)
+
+        producto = {}
+        producto['nombre'] = response.xpath("//aside/div/div/h5/text()").get()
+        producto['linea'] = response.xpath("//aside/div/div/h1/text()").get()
+        self.productos.append(producto)
+
+        yield {
+            'zapas' : self.productos
         }
+        
