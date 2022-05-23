b98d92d64f88ddd6157200742df36f8ae10c413f
AntonioPinar <antoniopinar2@gmail.com>
2022-05-23 15:24:36 +0200

    desabilitamos opcion de robots.txt

--- scrapeo_ropa/scrapeo_ropa/settings.py
+++ scrapeo_ropa/scrapeo_ropa/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'scrapeo_ropa.spiders'
 #USER_AGENT = 'scrapeo_ropa (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
