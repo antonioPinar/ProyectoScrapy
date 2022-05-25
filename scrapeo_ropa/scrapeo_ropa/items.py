494b85ee8df15b922ee0f6f0c14d0738947740d9
AntonioPinar <antoniopinar2@gmail.com>
2022-05-25 16:01:08 +0200

    creacion de objeto zapatillas

--- scrapeo_ropa/scrapeo_ropa/items.py
+++ scrapeo_ropa/scrapeo_ropa/items.py
@@ -9,4 +9,11 @@ import scrapy
 class ScrapeoRopaItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    marca = scrapy.Field() 
+    linea = scrapy.Field()  
+    modelo = scrapy.Field() 
+    descripcion = scrapy.Field()  
+    precio = scrapy.Field() 
+    fecha_salida = scrapy.Field() 
+    imagen = scrapy.Field() 
+    url = scrapy.Field()
