0ad704dab1ed0d82f9cbb77ddca67f18b46eb0f2
AntonioPinar <antoniopinar2@gmail.com>
2022-06-01 12:48:43 +0200

    permisos de pipelines

--- scrapeo_ropa/scrapeo_ropa/settings.py
+++ scrapeo_ropa/scrapeo_ropa/settings.py
@@ -23,7 +23,7 @@ CLOSESPIDER_PAGECOUNT = 3
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0"
 
 #importar a bbdd
-#ITEM_PIPELINES = {'scrapeo_ropa.pipelines.ScrapeoRopaPipeline': 300}
+ITEM_PIPELINES = {'scrapeo_ropa.pipelines.ScrapeoRopaPipeline': 300}
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
