2c6114ba5002a3abb62cac7858910ea0c7a86464
AntonioPinar <antoniopinar2@gmail.com>
2022-06-15 20:01:43 +0200

    eliminacion carpeta de la web

--- pagina_web/resources/views/usuario/viewUsuario.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="col-md-12 pl-0">
-    <div class="card card-custom">
-        <div class="card-body">
-            <!--begin: Datatable-->
-            <div class="datatable datatable-bordered datatable-head-custom" id="list_normal_user" style="width: 100%;"></div>
-        </div>
-    </div>
-</div>
